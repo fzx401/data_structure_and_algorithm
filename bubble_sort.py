c7108f79d2ee4db2292170b7a3f5047621fd1d88
FaizalFeng <fzx401@gmail.com>
2023-05-05 21:32:08 +0800

    bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+class BubbleSort:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def bubble_sort(array):
+        for i in range(len(array) - 1):  # i代表循环的轮数
+            for j in range(0, len(array) - i - 1):  # j代表从位置0开始到已确定的第i大的值之前的位置
+                if array[j] > array[j + 1]:
+                    temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+        return "The result of bubble sort is {}".format(array)
+
+
+print(BubbleSort.bubble_sort([7, 3, 5, 1, 2, 6]))
