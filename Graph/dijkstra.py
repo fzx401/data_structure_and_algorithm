6f127c9a62a9751ffc31cf9e031ebd7ff010effa
FaizalFeng <fzx401@gmail.com>
2023-05-18 15:20:49 +0800

    2023.5.18

--- Graph/dijkstra.py
+++ Graph/dijkstra.py
@@ -1,20 +1,21 @@
-import numpy as np
-import pandas as pd
-class Dijstra:
-    def dijstra(self, start, graph):
-        nodes = list(graph.keys())
-        distances = {node: float("inf") for node in graph.keys()}
-        distances[start] = 0
-        visited = []
 
-        while len(visited) < len(graph):
-            current_node = min([node for node in nodes if node not in visited], key=lambda x: distances[x])
+class Dijkstra:
+    def dijstra(self, start, g):
+        # 首先罗列出所有涉及到的点
+        nodes = list(g.keys())
+        diss = {n: float("inf") for n in g.keys()}
+        diss[start] = 0  # 构建一个字典，存储从起点到各点的距离，除起点外，各点距离都设为0
+        visited = []  # 构建已访问节点的列表，已访问节点实际上就是已确定起点到其最短距离的点
+
+        while len(visited) < len(g):
+            #  每轮贪心搜索，从距离起点最近且不在已访问节点的点中找出距离最短的作为探索节点
+            current_node = min([node for node in nodes if node not in visited], key=lambda x: diss[x])
             visited.append(current_node)
-            for ne in graph[current_node].keys():
-                distance = distances[current_node] + graph[current_node][ne]
-                if distance < distances[ne]:
-                    distances[ne] = distance
-        return distances
+            for ne in g[current_node].keys():
+                dis = diss[current_node] + g[current_node][ne]
+                if dis < diss[ne]:
+                    diss[ne] = dis
+        return diss
 
 
 # 示例图的邻接表表示
@@ -26,7 +27,7 @@ graph = {
 }
 
 start_node = 'A'
-d = Dijstra()
+d = Dijkstra()
 distances = d.dijstra(start_node, graph)
 
 # 输出最短距离
