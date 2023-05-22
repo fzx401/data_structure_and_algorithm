f4c3f70b905848bcac08f9a6d433ef5c133dc29a
FaizalFeng <fzx401@gmail.com>
2023-05-22 23:18:16 +0800

    2023.5.22

--- DynamicProgram/fibonacci.py
+++ DynamicProgram/fibonacci.py
@@ -1,11 +1,24 @@
-class Solution:
+"""生成斐波那契数列有两种可能的要求：
+1.输入参数n为生成数字的个数
+2.输入参数n为在数字n前的斐波那契数"""
+
+#  2
+class Solution2:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
         if n == 1:
             return 1
-        return self.fib(n-1) + self.fib(n - 2)
-
-
-s = Solution()
-print(s.fib(5))
+        return self.fib(n - 1) + self.fib(n - 2)
+#  1
+class Solution1:
+    def fib(self, n : list) -> int:
+        res = [0, 1]
+        if n <= 2:
+            return res
+        while len(res) < n:
+            i, j = res[-2], res[-1]
+            res.append(i+j)
+        return res
+s = Solution1()
+print(s.fib(15))
