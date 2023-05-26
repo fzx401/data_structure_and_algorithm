2c6a05657be2070ebdbda120cb0e65b0b666b7a2
FaizalFeng <fzx401@gmail.com>
2023-05-26 14:38:06 +0800

    2023.5.26

--- Search/floyd.py
+++ Search/floyd.py
@@ -1,4 +1,19 @@
 def flyod(graph):
     n = len(graph)
     dist = graph
-    
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][ j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    return dist
+
+INF = float('inf')
+graph = [
+    [0, 5, INF, 10],
+    [INF, 0, 3, INF],
+    [INF, INF, 0, 1],
+    [INF, INF, INF, 0]
+]
+res = flyod(graph)
+print(res)
