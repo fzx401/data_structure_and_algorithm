7750a176c5ff93fcfc277459be5f83e29588a319
FaizalFeng <fzx401@gmail.com>
2023-06-01 21:00:38 +0800

    将给定数组转化为大根堆

--- Data Structures/nums_to_heap.py
+++ Data Structures/nums_to_heap.py
@@ -1,13 +1,12 @@
 def nums_to_maxheap(nums):
-    i = 0
-    while i < len(nums):
-        if nums[i] > nums[int((i-1)/2)]:
+    for i in range(len(nums)):
+        while nums[i] > nums[int((i-1/2))]:
             tmp = nums[i]
             nums[i] = nums[int((i-1)/2)]
             nums[int((i-1)/2)] = tmp
-        i += 1
+            i = int((i-1)/2)
     return nums
 
 
-arr = [10, 4, 3, 5, 1]
+arr = [1, 4, 3, 5, 10]
 print(nums_to_maxheap(arr))
