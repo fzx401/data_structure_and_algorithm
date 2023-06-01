5f9c9459b63b1f7a72b73d4846f93d7972ab9f9b
FaizalFeng <fzx401@gmail.com>
2023-06-01 21:18:54 +0800

    重新理解堆化

--- Data Structures/heap.py
+++ Data Structures/heap.py
@@ -1,6 +1,10 @@
-import heapq
-
 def heapify(arr, n, i):
+    """
+    :param arr: Input array
+    :param n: The length of array
+    :param i: The index of  parent node
+    :return: None
+    """
     largest = i  # 初始化最大元素的索引为父节点 i
     left = 2 * i + 1  # 左子节点索引
     right = 2 * i + 2  # 右子节点索引
@@ -15,11 +19,12 @@ def heapify(arr, n, i):
 
     # 如果最大元素的索引不是父节点 i，则交换父节点与最大元素
     if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
+        arr[i], arr[largest] = arr[largest], arr[i]  # 进行这轮操作后，能保证这棵二叉树上的最大值到父节点上
 
         # 对交换后的子树递归进行堆化操作
+        # 只有父节点比两个子节点之间的某一个小的时候，才会继续往下
         heapify(arr, n, largest)
-nums = [4, 10, 3, 5, 1]
+nums = [4,10, 3, 5, 1]
 print(nums)
 heapify(nums, 5, 0)
 print(nums)
