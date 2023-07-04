c3363af9af5a7350874c590e2d97cc7b2860a092
faizalfeng <fzx401@gmail.com>
2023-07-04 11:20:11 +0800

    2023.7.4

---
+++ Leetcode/Offer/22.py
@@ -0,0 +1,21 @@
+from DataStructures.linked_list import Node, LinkList
+
+
+class Solution:
+    def getKthFromEnd(self, head: Node, k: int):
+        slow = head
+        fast = head
+        while fast.next and k > 1:
+            fast = fast.next
+            k -= 1
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        return slow
+
+
+if __name__ == '__main__':
+    test_linklist = LinkList([1, 2, 3, 4, 5])
+    s = Solution()
+    res = s.getKthFromEnd(test_linklist.head, 1)
+    pass
